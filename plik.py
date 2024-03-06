4a21255c25e07828108a726daab06caed856bd85
KarolCeg <149320445+KarolCeg@users.noreply.github.com>
2024-03-06 20:58:18 +0100

    Update plik.py

--- plik.py
+++ plik.py
@@ -7,25 +7,39 @@ f.close()
 #program czyta wszytskie linijki z tekstu
 
 
------------------------
+----------------------- liczenie ile razy co wystąpiło
 def literki(nazwa):
-    s = dict()
+    s =dict()
     for znak in nazwa:
-        if (znak in s.keys()):
+        s[znak] = 0
+    for znak in nazwa:
+        if(znak in s.keys()):
             s[znak] +=1
     return s
-
-f = open("tekst.txt", "r")
-linijka = f.readline()
-linijka
+linijka = "ala ma kota"
 print(literki(linijka))
-"""while linijka:
-    print(linijka)
-    linijka = f.readline()"""
+
+---------------- z pliku zczytanie i liczenie
+def literki(nazwa):
+    s =dict()
+    linijka = f.readline()
+    while linijka:
+        for znak in linijka:
+            s[znak] = 0
+        for znak in linijka:
+            if(znak in s.keys()):
+                s[znak] +=1
+        linijka = f.readline()
+    return s
+f = open("tekst.txt", "r")
+print(literki(f))
 f.close()
 
 
-def literki1(nazwa):
-    f = open("nazwa.txt", "r")
-    linijka = f.readline()
-    f.close
+
+
+
+
+
+
+
