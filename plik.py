f3a25da31bee04624549d2587099ff1d3532a235
KarolCeg <149320445+KarolCeg@users.noreply.github.com>
2024-03-06 15:37:23 +0100

    Update plik.py

--- plik.py
+++ plik.py
@@ -5,3 +5,27 @@ while linijka:
     linijka = f.readline()
 f.close()
 #program czyta wszytskie linijki z tekstu
+
+
+-----------------------
+def literki(nazwa):
+    s = dict()
+    for znak in nazwa:
+        if (znak in s.keys()):
+            s[znak] +=1
+    return s
+
+f = open("tekst.txt", "r")
+linijka = f.readline()
+linijka
+print(literki(linijka))
+"""while linijka:
+    print(linijka)
+    linijka = f.readline()"""
+f.close()
+
+
+def literki1(nazwa):
+    f = open("nazwa.txt", "r")
+    linijka = f.readline()
+    f.close
