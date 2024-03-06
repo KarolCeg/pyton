a0ea79b312582ce791df3a98fc934b97b5e3c198
KarolCeg <149320445+KarolCeg@users.noreply.github.com>
2024-03-06 22:50:56 +0100

    Update plik.py

--- plik.py
+++ plik.py
@@ -21,16 +21,17 @@ print(literki(linijka))
 
 ---------------- z pliku zczytanie i liczenie
 def literki(nazwa):
-    s =dict()
+    s = dict()
     linijka = nazwa.readline()
     while linijka:
         for znak in linijka:
-            s[znak] = 0
-        for znak in linijka:
-            if(znak in s.keys()):
-                s[znak] +=1
+            if znak in s:
+                s[znak] += 1
+            else:
+                s[znak] = 1
         linijka = nazwa.readline()
     return s
+
 f = open("tekst.txt", "r")
 print(literki(f))
 f.close()
